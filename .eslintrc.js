828d0274cbacb83b1209b4a498ab4ef273c66977
Meathill Zhai <zhailuaji@wps.cn>
2021-11-08 15:04:29 +0800

    change: got ready for publish

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "es2021": true,
+    commonjs: true,
+  },
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+  },
+  "extends": "eslint:recommended",
+};
